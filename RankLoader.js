7a3b32e3b542221223bfd7dc37cb0fddb01eb00a
VPRC-13 <coomonster13@gmail.com>
2026-07-15 23:06:27 -0500

    Add files via upload

---
+++ RankLoader.js
@@ -0,0 +1,15 @@
+function GetLR(Text){
+fetch("LowRanks.txt")
+.then(function(Response) {
+return Response.text();
+})
+.then(function(Text){
+document.getElementById("LowRanks").innerHTML = Text;
+alert("LOADEDLULlLLlll")
+});
+
+
+
+
+}
+GetLR();
